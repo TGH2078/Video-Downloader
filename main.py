8cd472efb85cac3a78831d87dbb4f260fb25a928
TGH2078 <84937725+TGH2078@users.noreply.github.com>
2021-05-27 22:27:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import requests
+import re
+from urllib.parse import unquote
+import sys
+import time
+def dlvid(url, name, pl, si):
+    #print("")
+    chunk_size = 1024*20
+    count = 0
+    r = requests.get(url, stream=True, timeout=3)
+    size = int(r.headers['Content-length'])
+    with open(name, "wb") as f:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            #count += chunk_size
+            count += f.write(chunk)
+            print("\033[1A" + name + " | " + "[" + "="*round(count/((int(size))/(si))) + ">" + " "*(si-round(count/((int(size))/(si)))) + "] " + ("   " + str(round(count/((int(size))/(100)))))[-3:] + "% | " + (" "*5 + str(round(((count/1024)/1024)*1000)/1000).split(".")[0] + "," + (str(round(((count/1024)/1024)*1000)/1000).split(".")[1] + "000")[0:3] + "/" + str(round(((size/1024)/1024)*1000)/1000).split(".")[0] + "," + (str(round(((size/1024)/1024)*1000)/1000).split(".")[1] + "000")[0:3])[-15:] + " MB | " + pl)
+        f.close()
+
+#=PLATFORMS=====================================================================
+def getvivo(url):
+    html = requests.get(url).text
+    t = re.search("(?s)InitializeStream\s*\(\s*(\{.+?})\s*\)\s*;", html).group().split("\n")
+    k = ""
+    for a in t:
+        if("source" in a):
+            k = a.replace("	", "").replace("source: '", "").replace("',", "")
+    r = unquote(k)
+    x = []
+    for i in range(len(r)):
+        j = ord(r[i])
+        if j >= 33 and j <= 126:
+            x.append(chr(33 + ((j + 14) % 94)))
+        else:
+            x.append(r[i])
+    return ''.join(x)
+
+def getsendfox(url):
+    html = requests.get(url).text
+    t = re.search("<textarea.*</textarea>", html).group()
+    t = re.search(">.*<", t).group().replace("<", "").replace(">", "")
+    return(t)
+#===============================================================================
+def getvideo(url):
+    if("vivo.sx" in url):
+        return(("OK", getvivo(url), "vivo.sx"))
+    #elif("sendfox.org" in url):
+    #    return(("OK", getsendfox(url), "sendfox.org"))
+    else:
+        return(("ERR", "", ""))
+
+def downloadvideo(url, output):
+    while(True):
+        try:
+            print("\033[2KStart Downloading " + url)
+            stream = getvideo(url)
+            if(stream[0]!="OK"):
+                print("\033[1A\033[2KError Dowloading " + url)
+                break
+            dlvid(stream[1], output, stream[2], 50)
+            break
+        except KeyboardInterrupt:
+            print("\033[2KKeyboardInterrupt")
+            break
+        except:
+            print("\033[1A\033[2KError Downloading, retry\033[1A")
+            time.sleep(1)
+
+def dlfile(file, folder):
+    try:
+        dllist = open(file).read().split("\n")
+        for a in dllist:
+            if(not a=="" and not a[0]=="#"):
+                n = a.split(", ")[0]
+                a.split(", ")[1]
+                downloadvideo(a.split(", ")[1], (folder+"/"+a.split(", ")[0]).replace("//", "/"))
+    except KeyboardInterrupt:
+        print("\033[2KKeyboardInterrupt")
+
+i = sys.argv[1:]
+mf = False
+file = False
+if("--help" in i or "-h" in i):
+    print("Download a single file with [url output.mp4]\nDownload from a Download-List with [-f dllist.txt ./video-folder]")
+    exit()
+for a in i:
+    if(a=="-f"):
+        mf = True
+    elif(mf):
+        file = a
+        mf = False
+if(not file==False):
+    dlfile(file, i[-1])
+else:
+    downloadvideo(i[-2], i[-1])
